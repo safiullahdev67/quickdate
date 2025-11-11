370a11ce08a7c643dc1acf1e5182ad1c4851dfca
umairanwarr <umairanwar321@yahoo.com>
2025-11-12 03:29:46 +0500

    minor fix

--- app/date-analytics/page.tsx
+++ app/date-analytics/page.tsx
@@ -1,11 +1,7 @@
-import dynamic from 'next/dynamic';
 import { requireAdmin } from '@/lib/auth';
+import DateAnalytics from '@/components/date-analytics/DateAnalytics';
 
 export const runtime = 'nodejs';
-
-const DateAnalytics = dynamic(() => import('@/components/date-analytics/DateAnalytics'), {
-  ssr: false,
-});
 
 export default async function DateAnalyticsPage() {
   await requireAdmin();
